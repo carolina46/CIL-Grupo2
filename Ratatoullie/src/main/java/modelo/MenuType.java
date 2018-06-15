f55caafdb726ca9bac23cb98cb6ccc9d538da2e7
carolina46 <mariacarola46@hotmail.com>
2018-06-15 14:41:24 -0300

    add class MenuType

---
+++ Ratatoullie/src/main/java/modelo/MenuType.java
@@ -0,0 +1,19 @@
+package modelo;
+
+/**
+ * Represent a label associated with a menu. Describe the grouping of your dishes.
+ * @author CIL-Grupo2
+ */
+
+
+public class MenuType {
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
