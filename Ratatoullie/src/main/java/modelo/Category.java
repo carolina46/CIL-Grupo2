4bcd34d3b92a414228ba1a10ddc7a827c577bf37
Carolina Perez <mariacarola46@hotmail.com>
2018-06-15 14:17:38 -0300

    add class Category

---
+++ Ratatoullie/src/main/java/modelo/Category.java
@@ -0,0 +1,24 @@
+package modelo;
+
+/**
+ * Represents a descriptive name that can be associated with one o more restaurants.
+ * @author CIL-Grupo2
+ */
+
+public class Category {
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
+
+
+
+
+
+
