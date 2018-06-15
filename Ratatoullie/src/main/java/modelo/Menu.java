70322127b2a6089ebc42436c6a781e8a41e6a85d
carolina46 <mariacarola46@hotmail.com>
2018-06-15 16:50:02 -0300

    complete the classes Menu and Restaurant

--- Ratatoullie/src/main/java/modelo/Menu.java
+++ Ratatoullie/src/main/java/modelo/Menu.java
@@ -1,11 +1,24 @@
 package modelo;
+import java.util.List;
 
 /**
- * Represents a menu of a restaurant. It has a number of dishes.
+ * Represents a menu of a restaurant. 
+ * Group a set of dishes.
+ * Can have labels that describe its contents.
  * 
  * @author CIL-Grupo2
  */
 
 public class Menu {
-
+	
+	private List<Dish> dishes;
+	private List<Tag> tags;
+	
+	public void addDish(Dish dish) {this.dishes.add(dish);}
+	public List<Dish> getDishes() { return dishes;}
+	public void setDishes(List<Dish> dishes) {this.dishes = dishes;}
+	
+	public void addTag(Tag tag) {this.tags.add(tag);}
+	public List<Tag> getTags() { return tags;}
+	public void setTags(List<Tag> tags) {this.tags = tags;}
 }
--- Ratatoullie/src/main/java/modelo/Restaurant.java
+++ Ratatoullie/src/main/java/modelo/Restaurant.java
@@ -11,12 +11,16 @@ import java.util.List;
 public class Restaurant {
 	
 	private String name;
+	private Category category;
 	private List<Menu> menus;
 	
 	
-	public String getName() {return name;}
-	public void setName(String name) {this.name = name;}
+	public String getName() { return name;}
+	public void setName(String name) { this.name = name;}
 	
+	public Category getCategory() { return category;}
+	public void setCategory(Category category) {this.category = category;}
+
 	public void addMenu(Menu m) {
 		this.menus.add(m);
 	}
