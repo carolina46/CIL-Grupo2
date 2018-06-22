aa0e1448afb0c7fc645278cacf49b6aa57cf8ab7
carolina46 <mariacarola46@hotmail.com>
2018-06-22 16:15:21 -0300

    add SystemRaratoullieTest class

---
+++ Ratatoullie/src/test/java/test/SystemRatatoullieTest.java
@@ -0,0 +1,51 @@
+package test;
+
+import modelo.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SystemRatatoullieTest {
+	
+	private SystemRatatouille sys;
+	private Normal normal_1;
+	private Responsible responsible1;
+	private Restaurant restaurant1;
+		
+
+	@BeforeEach
+	void setUp() throws Exception {
+		sys= SystemRatatouille.getSystemInstance();
+		normal_1= new Normal("Pepa", "Pepa001", "1234");
+		responsible1= new Responsible("Pepa", "Pepa001", "1234");
+		restaurant1= new Restaurant();
+		
+		
+	}
+	
+	
+	@Test
+	void userNameIsUniqueTest() {
+		Normal normal_2=new Normal("Marta", "marta001", "1234");
+		Normal normal_3=new Normal("Pepa", "Pepa001", "1234");
+		
+		//queremos agregar un usuario nuevo y uno repetido ¿para verificar trows o mensaje?
+		/*
+		assertTrows(sys.add(normal_2));
+		assertTrows(sys.add(normal_3));
+			*/			
+	}
+	
+
+
+	@Test
+	void commentsContinueExistTest() {
+		//agregar comentarios a un restaurante y luego eliminar la persona qe lo hizo
+		//comprobar que el comentario aun existe			
+	}
+
+	
+	
+}
