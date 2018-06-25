6bf0d27342338bcf09ebcfabd810c6c42a0e8ce5
Federico Pantaleone <fragsman@gmail.com>
2018-06-25 13:22:50 -0300

    Add constructor, add reference to restaurant

--- Ratatoullie/src/main/java/modelo/Comment.java
+++ Ratatoullie/src/main/java/modelo/Comment.java
@@ -12,9 +12,15 @@ import java.time.LocalDateTime;
 public class Comment {
 
 	private String description;
-	private LocalDateTime date; 
+	private LocalDateTime date;
+	private Restaurant restaurant;
 	
-
+	public Comment(String description, Restaurant restaurant) {
+		this.description = description;
+		this.restaurant = restaurant;
+		date = LocalDateTime.now();
+	}
+	
 	//Methods associated with Date
 	public LocalDateTime getDate() {	return date;}
 	public void setDate(LocalDateTime date) { this.date = date;}
@@ -23,5 +29,7 @@ public class Comment {
 	public String getDescription() { return description;}
 	public void setDescription(String description) { this.description = description;}
 	
-	
+	//Methods associated with Restaurant
+	public Restaurant getRestaurant() {		return restaurant;	}
+	public void setRestaurant(Restaurant restaurant) {	this.restaurant = restaurant;	}
 }
