9e3827e599e98671ea733fbceace87e4286a26c4
carolina46 <mariacarola46@hotmail.com>
2018-06-25 10:27:01 -0300

    add systemIsUniqueTest() to the class SystemRatatoullieTest

--- Ratatoullie/src/test/java/test/SystemRatatoullieTest.java
+++ Ratatoullie/src/test/java/test/SystemRatatoullieTest.java
@@ -11,19 +11,48 @@ class SystemRatatoullieTest {
 	
 	private SystemRatatouille sys;
 	private Normal normal_1;
-	private Responsible responsible1;
-	private Restaurant restaurant1;
-		
+	private Responsible responsible_1;
+	private Restaurant restaurant_1;
+	private Category category;
 
+	
+	
 	@BeforeEach
 	void setUp() throws Exception {
 		sys= SystemRatatouille.getSystemInstance();
-		normal_1= new Normal("Pepa", "Pepa001", "1234");
-		responsible1= new Responsible("Pepa", "Pepa001", "1234");
-		restaurant1= new Restaurant();
 		
+		//A normal user
+		normal_1= new Normal("Pepa", "Pepa001", "1234");
+		sys.addUser(normal_1);
+		
+		//A responsible user
+		responsible_1= new Responsible("Pepa", "Pepa001", "1234");
+		sys.addUser(responsible_1);
+		
+		//A restaurant
+		category = new Category("fast Food");
+		restaurant_1= new Restaurant("McDonals", category);
+		responsible_1.addRestaurant(restaurant_1);
+		sys.addRestaurant(restaurant_1);
 		
 	}
+	
+
+	//This test checks if there is a single instance of the system
+	@Test
+	void systemIsUniqueTest() {
+		SystemRatatouille sys2=	SystemRatatouille.getSystemInstance();
+		Normal normal_2= new Normal("Marta", "Marta57", "MartaPass");
+		
+		sys2.addUser(normal_2);
+		assertTrue(sys.numberOfUsers() == 3);
+		
+		sys.deleteRestaurant(restaurant_1);
+		assertTrue(sys2.numberOfRestaurants() == 0);
+		assertTrue(sys2.numberOfRestaurants() == sys.numberOfRestaurants());
+	}
+	
+
 	
 	
 	@Test
