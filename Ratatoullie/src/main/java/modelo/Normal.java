f7dde35077f27f26f9db07ecc2958bb130670f14
carolina46 <mariacarola46@hotmail.com>
2018-06-25 14:28:07 -0300

    fix warnings

--- Ratatoullie/src/main/java/modelo/Normal.java
+++ Ratatoullie/src/main/java/modelo/Normal.java
@@ -4,7 +4,6 @@ public class Normal extends Client {
 
 	public Normal(String fullName, String user, String password) {
 		super(fullName, user, password);
-		// TODO Auto-generated constructor stub
 	}
 
 }
