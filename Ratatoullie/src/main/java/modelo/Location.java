8088f3e74346c569615c7e6cab4c6dcc37a92a44
Federico Pantaleone <fragsman@gmail.com>
2018-06-30 14:23:50 -0300

    add Location class

---
+++ Ratatoullie/src/main/java/modelo/Location.java
@@ -0,0 +1,26 @@
+package modelo;
+
+/**
+ * Represents a Location given the Latitude and Longitude
+ * @author CIL-Grupo2
+ *
+ */
+
+public class Location {
+
+	private Double latitude;
+	private Double longitude;
+	
+	public Double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+	public Double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+}
