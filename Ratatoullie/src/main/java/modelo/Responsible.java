48f3ec1fd504b973e2ebc528121bd6cd9e74c6a7
Federico Pantaleone <fragsman@gmail.com>
2018-07-01 16:19:19 -0300

    removeed filter from Responsible. Added filter to Restaurant to match Class Diagram.

--- Ratatoullie/src/main/java/modelo/Responsible.java
+++ Ratatoullie/src/main/java/modelo/Responsible.java
@@ -13,7 +13,6 @@ import java.util.List;
 public class Responsible extends Client{
 	
 	private List<Restaurant> restaurants;
-	private CommentFilter filter;
 
 	public Responsible(String fullName, String user, String password, Location location) {
 		super(fullName, user, password, location);
@@ -29,13 +28,5 @@ public class Responsible extends Client{
 	}
 
 	public void addRestaurant(Restaurant restaurant) { this.restaurants.add(restaurant); }
-
-	public CommentFilter getFilter() {
-		return filter;
-	}
-
-	public void setFilter(CommentFilter filter) {
-		this.filter = filter;
-	}
 
 }
