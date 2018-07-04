2f5aabc06a1778677c45db4b84d2e89dae9b7e37
Federico Pantaleone <fragsman@gmail.com>
2018-07-04 13:28:44 -0300

    removed if from the for sentence making it more efficient and shorteninig the for

--- Ratatoullie/src/test/java/test/RakingTest.java
+++ Ratatoullie/src/test/java/test/RakingTest.java
@@ -28,12 +28,11 @@ class RakingTest{
 	void testFirstRanking() {
 		//This Test is for ensuring that passing of the Ranking from Visitor to Comensal its done correctly 
 		System.out.println("testFirstRanking()");
-		for(int i=1; i<22; i++) {
+		for(int i=1; i<21; i++) {
 			normal.comment("comentario"+ i, restaurant);
-			System.out.println("Execution number"+ i);
-			if(i == 20)
-				assertEquals(Comensal.class,normal.getRanking().getClass()); //when comments reach 20 Ranking changes to Comensal
+			System.out.println("Execution number"+ i);				
 		}
+		assertEquals(Comensal.class,normal.getRanking().getClass()); //when comments reach 20 Ranking changes to Comensal
 	}
 	
 	@Test
@@ -41,12 +40,11 @@ class RakingTest{
 		//This Test is for ensuring that passing of the Ranking from Comensal to Gourment its done correctly
 		System.out.println("testSecondRanking()");
 		
-		for(int i=22; i<42; i++) {
+		for(int i=22; i<41; i++) {
 			normal.comment("comentario"+ i, restaurant);
-			System.out.println("Execution number"+ i);
-			if(i==40)
-				assertEquals(Gourmet.class,normal.getRanking().getClass()); //when comments reach 40 Ranking changes to Gourmet	
+			System.out.println("Execution number"+ i);	
 		}
+		assertEquals(Gourmet.class,normal.getRanking().getClass()); //when comments reach 40 Ranking changes to Gourmet	
 	}
 
 	
