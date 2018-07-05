6ccba22da7ffaf933d6a82f43f98bb0cd45090c9
carolina46 <mariacarola46@hotmail.com>
2018-07-05 18:31:31 -0300

    add empty constructor in NormalClient for Hibernat. Add acomment for JavaDoc. Add comment about what we must implement

--- Ratatoullie/src/main/java/model/users/Administrator.java
+++ Ratatoullie/src/main/java/model/users/Administrator.java
@@ -1,9 +1,21 @@
 package model.users;
 
+import model.Comment;
+
+/**
+ * Represents a Administrator of the system.
+ * Can delete comments of the clients.
+ * Etc.
+ * @author CIL-Grupo2
+ */
+
 public class Administrator extends User {
+	
+	public Administrator() {} //For Hibernate
 
 	public Administrator(String fullName, String user, String password) {
 		super(fullName, user, password);
 	}
 
+	/*IMPLEMENTE*/ public void deleteComment(Client clente, Comment comment) {};
 }
