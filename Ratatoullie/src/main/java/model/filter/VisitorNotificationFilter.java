56f535657db265108b415e0a9f51410dae07767f
carolina46 <mariacarola46@hotmail.com>
2018-07-05 16:47:28 -0300

    Add comments javadoc. Add empty constructor in VisitorNotificationFilter for Hibernate. Fix some Comments

--- Ratatoullie/src/main/java/model/filter/VisitorNotificationFilter.java
+++ Ratatoullie/src/main/java/model/filter/VisitorNotificationFilter.java
@@ -4,25 +4,31 @@ import model.Comment;
 import model.Notification;
 import model.users.Responsible;
 
+/**
+ * Represents the VISITOR configuration for sending notifications to the restaurant responsible when there are new comments.
+ * Only notifications of clients of type Visitor are sent.
+ * @author CIL-Grupo2
+ */
+
 public class VisitorNotificationFilter extends NotificationFilter {
 
+	public VisitorNotificationFilter() {} //For Hibernate
+	
 	public VisitorNotificationFilter(Responsible responsible) {
 		super(responsible);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void applyNotificationForVisitor(Comment comment) {
-		//Creates notfication and adds into Responsible's notification list
-		Notification notification = new Notification();
-		notification.setComment(comment);
+		//Creates notification and adds into Responsible's notification list
+		Notification notification = new Notification(comment);
 		super.responsible.addNotification(notification);
 	}
 
 	@Override
-	public void applyNotificationForGourmet(Comment comment) { /*This is being called from a Visitor Ranking so it will NOT Notify */ }
+	public void applyNotificationForGourmet(Comment comment) { /*This is being called from a Gourmet Ranking so it will NOT Notify */ }
 
 	@Override
-	public void applyNotificationForComensal(Comment comment) { /*This is being called from a Visitor Ranking so it will NOT Notify */ }
+	public void applyNotificationForComensal(Comment comment) { /*This is being called from a Comensal Ranking so it will NOT Notify */ }
 
 }
