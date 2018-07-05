ed38c9f9d021664bda5b509ee62ff5756cd7a317
carolina46 <mariacarola46@hotmail.com>
2018-07-05 17:53:50 -0300

    Add empty constructor for Hibernate

--- Ratatoullie/src/main/java/model/users/User.java
+++ Ratatoullie/src/main/java/model/users/User.java
@@ -9,42 +9,38 @@ package model.users;
 
 public abstract class User {
 	
-	private Long oid;
+	private Long oid; //For Hibernate
 	private String fullName;
 	private String user;
 	private String password;
 		
+	
+	public User() {}  //For Hibernate
+	
 	public User(String fullName, String user, String password) {
 		this.fullName = fullName;
 		this.user = user;
 		this.password = password;
 	}
 	
-	public Long getOid() {
-		return oid;
-	}
-
-	public void setOid(Long oid) {
-		this.oid = oid;
-	}
 	
-	public String getFullName() {
-		return fullName;
-	}
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
-	public String getUser() {
-		return user;
-	}
-	public void setUser(String user) {
-		this.user = user;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	public Long getOid() { return oid; }
+
+	public void setOid(Long oid) { this.oid = oid; }
+	
+	
+	public String getFullName() { return fullName; }
+	
+	public void setFullName(String fullName) { this.fullName = fullName; }
+	
+	
+	public String getUser() { return user;	}
+	
+	public void setUser(String user) { this.user = user; }
+	
+	
+	public String getPassword() { return password; }
+	
+	public void setPassword(String password) { this.password = password; }
 	
 }
