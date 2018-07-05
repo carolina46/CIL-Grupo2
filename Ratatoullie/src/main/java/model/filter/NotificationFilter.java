3d76621ebef60d09573796df53f6eaa4f4e06fb6
carolina46 <mariacarola46@hotmail.com>
2018-07-05 15:55:58 -0300

    Add Empty contructor in class NotificationFilter for hibernate

--- Ratatoullie/src/main/java/model/filter/NotificationFilter.java
+++ Ratatoullie/src/main/java/model/filter/NotificationFilter.java
@@ -10,23 +10,31 @@ import model.users.Responsible;
 
 public abstract class NotificationFilter {
 	
-	private Long oid;
+	private Long oid; //For Hibernate
 	protected Responsible responsible;
 	
+	public NotificationFilter() {} //For Hibernate
+	
 	public NotificationFilter(Responsible responsible) {
+		super();
 		this.responsible = responsible;
 	}
 	
-	public Long getOid() {	return oid;	}
+	public Long getOid() { return oid;	}
+	
 	public void setOid(Long oid) { this.oid = oid;	}
 	
+	
 	public Responsible getResponsible() { return responsible;}
+	
 	public void setResponsible(Responsible responsible) { this.responsible = responsible;}
 
-	/*
-	 * They are used to determine if the restaurant responsible should be notified of a new comment
-	 */
+	
+	//They are used to determine if the restaurant responsible should be notified of a new comment
+	 	
 	public abstract void applyNotificationForVisitor(Comment coment);
+	
 	public abstract void applyNotificationForGourmet(Comment coment);
+	
 	public abstract void applyNotificationForComensal(Comment coment);	
 }
