d968ea6dca910dc37a08ea07f9878ea9528701b3
carolina46 <mariacarola46@hotmail.com>
2018-07-05 16:09:58 -0300

    Comments for JavaDocs and Add Empty contructor in class NotificationFilter for hibernate

--- Ratatoullie/src/main/java/model/filter/DenyNotificationFilter.java
+++ Ratatoullie/src/main/java/model/filter/DenyNotificationFilter.java
@@ -3,15 +3,21 @@ package model.filter;
 import model.Comment;
 import model.users.Responsible;
 
+/**
+ * Represents the DENY configuration. No notification will be sent
+ * @author CIL-Grupo2
+ */
+
 public class DenyNotificationFilter extends NotificationFilter {
+	
+	public DenyNotificationFilter() {} //For Hibernate
 
 	public DenyNotificationFilter(Responsible responsible) {
 		super(responsible);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void applyNotificationForVisitor(Comment comment) { }
+	public void applyNotificationForVisitor(Comment comment) {}
 
 	@Override
 	public void applyNotificationForGourmet(Comment comment) {}
