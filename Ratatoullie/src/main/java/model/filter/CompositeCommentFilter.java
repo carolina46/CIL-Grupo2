8baa8684efab3337461d8aad935ba6298fd1674e
carolina46 <mariacarola46@hotmail.com>
2018-07-05 16:42:38 -0300

    Add Comments in CompositeCommentFilter

--- Ratatoullie/src/main/java/model/filter/CompositeCommentFilter.java
+++ Ratatoullie/src/main/java/model/filter/CompositeCommentFilter.java
@@ -13,25 +13,23 @@ public class CompositeCommentFilter extends CommentFilter {
 	
 	private ArrayList<CommentFilter> configurationFilters;
 	
+	
 	public CompositeCommentFilter() {
 		super();
 		configurationFilters = new ArrayList<CommentFilter>();
 	}
 
-	public void addFilter(CommentFilter configurationFilter){
-		configurationFilters.add(configurationFilter);
-	}
+	
+	public void addFilter(CommentFilter configurationFilter){ configurationFilters.add(configurationFilter);}
 
-	public ArrayList<CommentFilter> getConfigurationFilters() {
-		return configurationFilters;
-	}
+	public ArrayList<CommentFilter> getConfigurationFilters() {	return configurationFilters;}
 
-	public void setConfigurationFilters(ArrayList<CommentFilter> configurationFilter) {
-		this.configurationFilters = configurationFilter;
-	}
+	public void setConfigurationFilters(ArrayList<CommentFilter> configurationFilter) {	this.configurationFilters = configurationFilter;}
 
+	
 	@Override
 	public boolean confirmVisitorComment() {
+		//It's being called from a Visitor Ranking, it will go through all Configurations but only allows comment if VisitorConfiguration is present
 		for (CommentFilter filter : this.configurationFilters)
 			if(filter.confirmVisitorComment()) return true;
 		return false;
@@ -39,6 +37,7 @@ public class CompositeCommentFilter extends CommentFilter {
 
 	@Override
 	public boolean confirmGourmetComment() {
+		//It's being called from a Gourmet Ranking, it will go through all Configurations but only allows comment if GourmetConfiguration is present
 		for (CommentFilter filter : this.configurationFilters)
 			if(filter.confirmGourmetComment()) return true;
 		return false;
@@ -46,11 +45,10 @@ public class CompositeCommentFilter extends CommentFilter {
 
 	@Override
 	public boolean confirmComensalComment() {
+		//It's being called from a Comensal Ranking, it will go through all Configurations but only allows comment if ComensalConfiguration is present
 		for (CommentFilter filter : this.configurationFilters)
 			if(filter.confirmComensalComment()) return true;
 		return false;
 	}
-
-
 
 }
