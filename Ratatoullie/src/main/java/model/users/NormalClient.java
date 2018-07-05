ad26a8cbb3bc3468a22bba6f53b019d7dc185462
carolina46 <mariacarola46@hotmail.com>
2018-07-05 18:24:02 -0300

    add empty constructor in NormalClient for Hibernat. Add acomment for JavaDoc

--- Ratatoullie/src/main/java/model/users/NormalClient.java
+++ Ratatoullie/src/main/java/model/users/NormalClient.java
@@ -2,7 +2,14 @@ package model.users;
 
 import model.business.Location;
 
+/**
+ * Represents a user of the system can comment on a restaurant and vote or recommend a restaurant, menu or dish.
+  * @author CIL-Grupo2
+ */
+
 public class NormalClient extends Client {
+	
+	public NormalClient() {} //For Hibernate
 
 	public NormalClient(String fullName, String user, String password, Location location) {
 		super(fullName, user, password, location);
