e29c36d99384ea6c9533f9b43f7b25a42293b005
carolina46 <mariacarola46@hotmail.com>
2018-07-05 17:04:18 -0300

    Add javaDocs Comments and empty constructor in visitor class for Hibernate

--- Ratatoullie/src/main/java/model/ranking/Visitor.java
+++ Ratatoullie/src/main/java/model/ranking/Visitor.java
@@ -14,6 +14,8 @@ import model.users.Client;
 
 public class Visitor extends Ranking {
 
+	public Visitor() {} //For Hibernate
+	
 	/**
 	 * Change the user's state to comensal depending on the total of comments made.
 	 * @param Receives the user whom it belongs.
@@ -22,16 +24,21 @@ public class Visitor extends Ranking {
 	public void updateRanking(Client c) {
 		if(c.numberOfComments()>19) {
 			c.setRanking(new Comensal());
-			
 		}
 	}
 
+	/**
+	 * Inform to the NotificationFilter of a restaurant about a new comment from a visitor client.
+	 */
 	@Override
 	public void notifyNewCommentAbout(Restaurant restaurant, Comment comment) {
 		restaurant.getNotificationFilter().applyNotificationForVisitor(comment);
 		
 	}
 
+	/**
+	 * Check whit the CommentFilter of a restaurant if a client Visitor can comment.
+	 */
 	@Override
 	public boolean canCommentAbout(Restaurant restaurant) {
 		return restaurant.getCommentFilter().confirmVisitorComment();
