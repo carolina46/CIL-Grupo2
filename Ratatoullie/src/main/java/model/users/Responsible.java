cbfd926d70f647323205aaa4b103075be6cb9a9d
Federico Pantaleone <fragsman@gmail.com>
2018-07-04 22:27:50 -0300

    added notifications relationship to Responsible

--- Ratatoullie/src/main/java/model/users/Responsible.java
+++ Ratatoullie/src/main/java/model/users/Responsible.java
@@ -3,6 +3,7 @@ package model.users;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.Notification;
 import model.business.Location;
 import model.business.Restaurant;
 
@@ -16,11 +17,12 @@ import model.business.Restaurant;
 public class Responsible extends Client{
 	
 	private List<Restaurant> restaurants;
+	private List<Notification> notfications;
 
-	
 	public Responsible(String fullName, String user, String password, Location location) {
 		super(fullName, user, password, location);
-		this.restaurants = new ArrayList<Restaurant>();
+		restaurants = new ArrayList<Restaurant>();
+		notfications = new ArrayList<Notification>();
 	}
 	
 	public List<Restaurant> getRestaurants() { return restaurants; }
@@ -34,4 +36,12 @@ public class Responsible extends Client{
 		//Determine what the notifications will be
 	}
 
+	public List<Notification> getNotfications() {
+		return notfications;
+	}
+
+	public void setNotfications(List<Notification> notfications) {
+		this.notfications = notfications;
+	}
+	
 }
