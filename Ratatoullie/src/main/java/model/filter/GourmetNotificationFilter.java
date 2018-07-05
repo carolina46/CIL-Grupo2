bf2637d71aa5a615691d70fb2e3d39d98a2b95b4
carolina46 <mariacarola46@hotmail.com>
2018-07-05 16:26:45 -0300

    add comments for Javadocs. Add empty constructor in GourmetNotificationFilter for Hibernate. Fix comments

--- Ratatoullie/src/main/java/model/filter/GourmetNotificationFilter.java
+++ Ratatoullie/src/main/java/model/filter/GourmetNotificationFilter.java
@@ -4,25 +4,31 @@ import model.Comment;
 import model.Notification;
 import model.users.Responsible;
 
+/**
+ * Represents the GOURMET configuration for sending notifications to the restaurant responsible when there are new comments.
+ * Only notifications of clients of type Gourmet are sent.
+ * @author CIL-Grupo2
+ */
+
 public class GourmetNotificationFilter extends NotificationFilter {
+	
+	public GourmetNotificationFilter() {} //For Hibernate
 
 	public GourmetNotificationFilter(Responsible responsible) {
 		super(responsible);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void applyNotificationForVisitor(Comment comment) { /*This is being called from a Gourmet Ranking so it will NOT Notify */ }
+	public void applyNotificationForVisitor(Comment comment) { /*This is being called from a Visitor Ranking so it will NOT Notify */ }
 
 	@Override
 	public void applyNotificationForGourmet(Comment comment) { 
-		//Creates notfication and adds into Responsible's notification list
-		Notification notification = new Notification();
-		notification.setComment(comment);
+		//Creates notification and adds into Responsible's notification list
+		Notification notification = new Notification(comment);
 		super.responsible.addNotification(notification);
 	}
 
 	@Override
-	public void applyNotificationForComensal(Comment comment) { /*This is being called from a Gourmet Ranking so it will NOT Notify */ }
+	public void applyNotificationForComensal(Comment comment) { /*This is being called from a Comensal Ranking so it will NOT Notify */ }
 
 }
