affc4e1bab289c3bb84174da49e9a60cab6c601d
carolina46 <mariacarola46@hotmail.com>
2018-07-05 13:46:34 -0300

    Add Empty contructor for hibernate and method addVote() in class Dish

--- Ratatoullie/src/main/java/model/business/Dish.java
+++ Ratatoullie/src/main/java/model/business/Dish.java
@@ -13,12 +13,14 @@ import model.vote.DishVote;
 
 public class Dish {
 		
-	private Long oid;
+	private Long oid; //For Hibernate
 	private String name;
 	private String description;
 	private String picture;
-	private List<Tag> tags; //en monguillot use ArrayList (ver diferencias y borrar este comentario)
+	private List<Tag> tags; 
 	private List<DishVote> dishVotes;
+	
+	public Dish() {} //For Hibernate
 	
 	public Dish(String name, String description, String picture) {
 		this.name = name;
@@ -28,44 +30,38 @@ public class Dish {
 		this.dishVotes = new ArrayList<DishVote>();
 	}
 	
-	public Long getOid() {
-		return oid;
-	}
-
-	public void setOid(Long oid) {
-		this.oid = oid;
-	}
 	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public String getPicture() {
-		return picture;
-	}
-	public void setPicture(String picture) {
-		this.picture = picture;
-	}
+	public Long getOid() { return oid;}
 
-	//Methods associated with Tag
-	public List<Tag> getTags() { return tags;}
+	public void setOid(Long oid) { this.oid = oid;}
+	
+	public String getName() { return name; }
+	
+	public void setName(String name) { this.name = name;}
+	
+	public String getDescription() { return description;}
+	
+	public void setDescription(String description) { this.description = description; }
+	
+	public String getPicture() { return picture; }
+	
+	public void setPicture(String picture) { this.picture = picture; }
+
+	//Methods associated with list of Tag
+	
+	public List<Tag> getTags() { return tags;} 
+	
 	public void addTag(Tag tag) {this.tags.add(tag);}
+	
 	public void setTags(List<Tag> tags) {this.tags = tags;}
 	
+
+	//Methods associated with list of votes
 	
-	public List<DishVote> getDishVotes() {
-		return dishVotes;
-	}
-	public void setDishVotes(List<DishVote> dishVotes) {
-		this.dishVotes = dishVotes;
-	}
+	public List<DishVote> getDishVotes() { return dishVotes;}
+	
+	public void setDishVotes(List<DishVote> dishVotes) { this.dishVotes = dishVotes;}
+	
+	public void addVote(DishVote dishVote) {this.dishVotes.add(dishVote);}
 	
 }
