2b5683f4fd9e1e9dcf914055fd85f27185668a93
carolina46 <mariacarola46@hotmail.com>
2018-07-05 14:44:49 -0300

    Add atribute oid for hibernate and mark pending methods to implement (Class SystemRatatouille)

--- Ratatoullie/src/main/java/model/SystemRatatouille.java
+++ Ratatoullie/src/main/java/model/SystemRatatouille.java
@@ -14,18 +14,19 @@ import model.users.User;
 
 public class SystemRatatouille {
 	
-	private Long oid;
+	private Long oid; // For Hibernate
 	private List<User> users;
 	private	List<Restaurant> restaurants;
 	private static SystemRatatouille uniqueSystem;
 		
 	//Private constructor, do not allow to generate default constructor.
 	private SystemRatatouille() {
+		super();
 		this.users = new ArrayList<User>();
 		this.restaurants = new ArrayList<Restaurant>();
 	}
 	
-	///get a system instance
+	///Get a system instance
 	public static SystemRatatouille getSystemInstance() {
 		if(uniqueSystem==null) {
 			uniqueSystem= new SystemRatatouille();
@@ -36,39 +37,53 @@ public class SystemRatatouille {
 		return uniqueSystem;
 	}
 	
-	public Long getOid() {
-		return oid;
-	}
+	
+	public Long getOid() { return oid; }
 
-	public void setOid(Long oid) {
-		this.oid = oid;
-	}
-	//Methods associated with Users
+	public void setOid(Long oid) { this.oid = oid; }
+	
+	
+	//Methods associated with list of Users
+	
 	public List<User> getUsers() { return users;}
-	public void addUser(User user) {this.users.add(user);}
+	
 	public int numberOfUsers() {return this.users.size();}
-	
-	
-	
-	public void setUsers(List<User> users) {this.users = users;}
 		
-	//Methods associated with restaurants
+	
+			//PENDING IMPLEMENTATION 
+			public void setUsers(List<User> users) {
+				//for each element must verify that it is unique in the system
+				this.users = users;
+			}
+		
+			
+			public void addUser(User user) {
+				//must verify that it is unique in the system
+				this.users.add(user);}
+				
+			public void deleteUser(User user) {
+				
+				//Must delete the User passed as a parameter
+				
+			}
+
+	
+	//Methods associated with list of restaurants
+	
 	public List<Restaurant> getRestaurants() { return this.restaurants;}
+	
 	public void setRestaurants(List<Restaurant> restaurants) {this.restaurants = restaurants;}
+	
 	public void addRestaurant(Restaurant restaurant) {this.restaurants.add(restaurant);}
+	
 	public int numberOfRestaurants() { return this.restaurants.size();}
 	
-	public void deleteRestaurant(Restaurant restaurant_1) {
-		
-		//Must delete the restaurant passed as a parameter 
-		
-	}
-
-	public void deleteUser(User normal_1) {
-		
-		//Must delete the User passed as a parameter
-		
-	}
+			//PENDING IMPLEMENTATION 
+			public void deleteRestaurant(Restaurant restaurant) {
+				
+				//Must delete the restaurant passed as a parameter 
+				
+			}
 
 		
 }
