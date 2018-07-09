9208aa4f8cc3a083291a035a6fb9f289c2df1f34
carolina46 <mariacarola46@hotmail.com>
2018-07-09 18:08:53 -0300

    Add TagDAO - No implementation

---
+++ Ratatoullie/src/main/java/components/daos/TagDAO.java
@@ -0,0 +1,45 @@
+package components.daos;
+
+import java.util.List;
+
+import model.business.Tag;
+
+public class TagDAO implements GenericDAO<Tag>{
+
+	@Override
+	public List<Tag> getList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Tag getByText(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Tag t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Tag t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modify(Tag t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getByID(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
