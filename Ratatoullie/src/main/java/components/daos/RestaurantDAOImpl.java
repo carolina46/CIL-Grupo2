84fac74c9179bd99057d3fb6e22518b7a590c7fc
Federico Pantaleone <fragsman@gmail.com>
2018-07-09 16:46:26 -0300

    completed basic DAO for restaurant

---
+++ Ratatoullie/src/main/java/components/daos/RestaurantDAOImpl.java
@@ -0,0 +1,51 @@
+package components.daos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Transaction;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import model.business.Restaurant;
+
+public class RestaurantDAOImpl implements RestaurantDAO{
+
+	private SessionFactory sessionFactory;
+	
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory =  sessionFactory;
+	}
+		
+	@Override
+	public void save(Restaurant restaurant) {
+		Session session = this.sessionFactory.openSession();
+			
+		try{
+			Transaction tx = session.beginTransaction();
+			session.save(restaurant);
+			tx.commit();
+		}catch(Exception e){
+			System.out.println("Error saving Restaurant");
+		}finally{
+			session.close();
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Restaurant> list() {
+		Session session = this.sessionFactory.openSession();
+		
+		List<Restaurant> restaurants = new ArrayList<Restaurant>();
+		try{
+			
+			restaurants = session.createQuery("from Restaurant").list();
+		}catch(Exception e){
+			System.out.println("Error retrieving Restaurants list");
+		}finally{
+			session.close();
+		}
+		return restaurants;
+	}
+}
--- Ratatoullie/src/main/resources/RestaurantDAO.java
+++
@@ -1,7 +0,0 @@
-
-public interface RestaurantDAO {
-
-	public void save(Restaurant restaurant) {};
-	
-	public List<Restaurant> list(){};
-}
