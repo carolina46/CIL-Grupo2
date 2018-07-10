ca8dc81829a7f838667dae20241ac25c8a5050c6
carolina46 <mariacarola46@hotmail.com>
2018-07-09 23:33:32 -0300

    problem with ModelAndView

--- Ratatoullie/src/main/java/components/controllers/RestaurantController.java
+++ Ratatoullie/src/main/java/components/controllers/RestaurantController.java
@@ -1,10 +1,21 @@
 package components.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
+@Controller
+@RequestMapping(value = "/restaurant")
 public class RestaurantController {
+	
+	@GetMapping(value = "/restaurant/{restaurantId}")
+	public ModelAndView getRestaurantProfile(@PathVariable Long restaurantId) {
+		ModelAndView modelAndView=new ModelAndView("restaurantProfile"); 
+		return modelAndView;
+	}
 	
 }
