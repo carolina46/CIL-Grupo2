606c2c66b5ab3095fe67ca270faba54d4ae8e216
Federico Pantaleone <fragsman@gmail.com>
2018-07-11 13:43:29 -0300

    implemented modify, save and delete methods

--- Ratatoullie/src/main/java/components/daos/HibernateUtil.java
+++ Ratatoullie/src/main/java/components/daos/HibernateUtil.java
@@ -1,7 +1,5 @@
 package components.daos;
 
-import java.io.Serializable;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -18,22 +16,101 @@ public class HibernateUtil {
 		super();
 	}
 	
-	public <T> Serializable save(final T entity) {		
+	public <T> boolean save(final T entity) {	
+		//Saves a generic the object entity
 		Session session = this.sessionFactory.openSession();
-		Serializable savedObject = null;
+		boolean saved = true;
 		
 		try{
 			Transaction tx = session.beginTransaction();
-			savedObject = session.save(entity);
+			session.save(entity);
 			tx.commit();
 		}catch(Exception e){
-			System.out.println("Error saving "+entity.toString());
+			System.out.println("HibernateUtil: Error saving "+entity.toString());
+			saved = false;
 		}finally{
 			session.close();
 		}
 		
-		return savedObject;
+		return saved;
 	}
+		
+	public <T> boolean delete(T entity) {
+		//Deletes a tuple from generic object T
+		Session session = this.sessionFactory.openSession();
+		boolean deleted = true;
+		
+		try{
+			Transaction tx = session.beginTransaction();
+			
+			session.delete(entity);
+			tx.commit();
+		}catch(Exception e){
+			System.out.println("HibernateUtil: Error deleting "+entity.toString());
+			deleted = false;
+		}finally{
+			session.close();
+		}
+		
+		return deleted;
+	}
+
+	public <T> boolean modify(final T entity) {
+		//Modifies the generic entity
+		Session session = this.sessionFactory.openSession();
+		boolean modified = true;
+		
+		try{
+			Transaction tx = session.beginTransaction();
+			
+			session.update(entity);
+			tx.commit();
+		}catch(Exception e){
+			System.out.println("HibernateUtil: Error modifying "+entity.toString());
+			modified = false;
+		}finally{
+			session.close();
+		}
+		
+		return modified;
+	}
+	
+	/*
+	@SuppressWarnings("unchecked")
+	public <T> List<T> getList() {
+		//Returns a list of the generic object T
+		Session session = this.sessionFactory.openSession();
+		
+		List<T> restaurants = new ArrayList<T>();
+		try{
+			
+			restaurants = session.createQuery("from T").list();
+		}catch(Exception e){
+			System.out.println("HibernateUtil: Error retrieving a list");
+		}finally{
+			session.close();
+		}
+		return restaurants;
+	}
+	*/
+	
+	/*
+	public String getByID(Long id) {
+		//Returns the generic object Type T searching by its ID
+		
+		Session session = this.sessionFactory.openSession();
+		try{
+			Transaction tx = session.beginTransaction();
+			session.load(entity);
+			tx.commit();
+		}catch(Exception e){
+			System.out.println("HibernateUtil: Error deleting "+entity.toString());
+			deleted = false;
+		}finally{
+			session.close();
+		}
+		return object;
+	}*/
 	
 	
 
