1d50c18d3363ee8c3e1ff4bcd99d62d86b8d65d7
Federico Pantaleone <fragsman@gmail.com>
2018-07-12 18:50:08 -0300

    fix problem with HibernateTest

--- Ratatoullie/src/main/java/HibernateTest.java
+++ Ratatoullie/src/main/java/HibernateTest.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import components.daos.RestaurantDAO;
+import components.daos.RestaurantDAOImpl;
 import model.business.Restaurant;
 
 public class HibernateTest {
@@ -12,7 +12,7 @@ public class HibernateTest {
 		
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ratatoullie-dispatcher-servlet.xml");
 		
-		RestaurantDAO resrautantDAO = context.getBean(RestaurantDAO.class);
+		RestaurantDAOImpl resrautantDAO = context.getBean(RestaurantDAOImpl.class);
 		
 		Restaurant restaurant = new Restaurant();
 		restaurant.setName("Starbucks");
@@ -20,7 +20,7 @@ public class HibernateTest {
 		resrautantDAO.save(restaurant);
 		
 		
-		List<Restaurant> list = resrautantDAO.list();
+		List<Restaurant> list = resrautantDAO.getAll();
 		
 		for(Restaurant r : list){
 			System.out.println("Restaurant List::"+r);
