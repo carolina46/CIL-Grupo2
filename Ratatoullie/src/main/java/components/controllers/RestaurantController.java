c036f3f0da6a22dd49cf4f8d6dee66607a00c345
carolina46 <mariacarola46@hotmail.com>
2018-07-13 13:19:34 -0300

    change some lines in RestaurantController

--- Ratatoullie/src/main/java/components/controllers/RestaurantController.java
+++ Ratatoullie/src/main/java/components/controllers/RestaurantController.java
@@ -4,21 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
-@RequestMapping(value = "/restaurant")
+//@RequestMapping(value = "/restaurant")
 public class RestaurantController {
 	
+	/*
 	@GetMapping
 	//(value = "/restaurant/{restaurantId}")
 	public ModelAndView getRestaurantProfile(@PathVariable Long restaurantId) {
 		ModelAndView modelAndView=new ModelAndView("restaurantProfile"); 
 		return modelAndView;
-	}
+	}*/
 	
-	@GetMapping(value = "/Restaurant")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView getIndex(@PathVariable Long restaurantId) {
 		ModelAndView modelAndView=new ModelAndView("index"); 
 		return modelAndView;
