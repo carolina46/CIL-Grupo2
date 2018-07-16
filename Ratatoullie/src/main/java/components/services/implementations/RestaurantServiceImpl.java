021ed6f51bf5254cca9bc445036f88fde25064e5
carolina46 <mariacarola46@hotmail.com>
2018-07-16 03:01:32 -0300

    Add RestaurantService and RestaurantServiceImpl

---
+++ Ratatoullie/src/main/java/components/services/implementations/RestaurantServiceImpl.java
@@ -0,0 +1,51 @@
+package components.services.implementations;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import components.daos.interfaces.RestaurantDAO;
+import components.services.interfaces.RestaurantService;
+import model.business.Restaurant;
+
+@Service
+public class RestaurantServiceImpl implements RestaurantService {
+
+	private RestaurantDAO restaurantDAO;
+	
+	public RestaurantServiceImpl() {}
+	
+	@Override
+	public boolean saveRestaurant(Restaurant restaurant) {
+		return restaurantDAO.save(restaurant);
+	}
+
+	@Override
+	public boolean updateRestaurant(Restaurant restaurant) {
+		return restaurantDAO.update(restaurant);
+	}
+
+	@Override
+	public boolean removeRestaurant(Restaurant restaurant) {
+		return restaurantDAO.remove(restaurant);
+	}
+
+	@Override
+	public boolean removeRestaurantById(Long id) {
+		return restaurantDAO.removeById(id);
+	}
+
+	@Override
+	public Restaurant getRestuarantByID(Long id) {
+		return restaurantDAO.getByID(id);
+	}
+
+	@Override
+	public Restaurant getRestaurantByName(String name) {
+		return restaurantDAO.getByName(name);
+	}
+
+	@Override
+	public List<Restaurant> getAllRestaurants() {
+		return restaurantDAO.getAll();
+	}
+
+}
---
+++ Ratatoullie/src/main/java/components/services/interfaces/RestaurantService.java
@@ -0,0 +1,15 @@
+package components.services.interfaces;
+
+import java.util.List;
+import model.business.Restaurant;
+
+public interface RestaurantService {
+	
+	public boolean saveRestaurant(Restaurant restaurant);
+	public boolean updateRestaurant(Restaurant restaurant);
+	public boolean removeRestaurant(Restaurant restaurant);
+	public boolean removeRestaurantById(Long id);
+	public Restaurant getRestuarantByID(Long id);
+	public Restaurant getRestaurantByName(String name);
+	public List<Restaurant> getAllRestaurants();
+}
