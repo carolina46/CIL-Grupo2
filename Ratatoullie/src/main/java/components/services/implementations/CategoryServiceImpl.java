78036074c93a58fd9a016bea9eae670c074c0f1b
carolina46 <mariacarola46@hotmail.com>
2018-07-18 16:09:54 -0300

    add CategoryService (interface + implementation)

---
+++ Ratatoullie/src/main/java/components/services/implementations/CategoryServiceImpl.java
@@ -0,0 +1,56 @@
+package components.services.implementations;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import components.daos.interfaces.CategoryDAO;
+import components.services.interfaces.CategoryService;
+import model.business.Category;
+import model.business.Category;
+
+@Service
+@Transactional
+public class CategoryServiceImpl implements CategoryService{
+
+	private CategoryDAO categoryDAO;
+	
+	public CategoryServiceImpl() {}
+	
+	@Override
+	public boolean saveCategory(Category Category) {
+		return categoryDAO.save(Category);
+	}
+
+	@Override
+	public boolean updateCategory(Category Category) {
+		return categoryDAO.update(Category);
+	}
+
+	@Override
+	public boolean removeCategory(Category Category) {
+		return categoryDAO.remove(Category);
+	}
+
+	@Override
+	public boolean removeCategoryById(Long id) {
+		return categoryDAO.removeById(id);
+	}
+
+	@Override
+	public Category getCategoryByID(Long id) {
+		return categoryDAO.getByID(id);
+	}
+
+	@Override
+	public Category getCategoryByName(String name) {
+		return categoryDAO.getCategory(name);
+	}
+
+	@Override
+	public List<Category> getAllCategorys() {
+		return categoryDAO.getAll();
+	}
+
+}
---
+++ Ratatoullie/src/main/java/components/services/interfaces/CategoryService.java
@@ -0,0 +1,16 @@
+package components.services.interfaces;
+
+import java.util.List;
+import model.business.Category;
+
+public interface CategoryService {
+	
+	public boolean saveCategory(Category category);
+	public boolean updateCategory(Category category);
+	public boolean removeCategory(Category category);
+	public boolean removeCategoryById(Long id);
+	public Category getCategoryByID(Long id);
+	public Category getCategoryByName(String name);
+	public List<Category> getAllCategorys();
+
+}
