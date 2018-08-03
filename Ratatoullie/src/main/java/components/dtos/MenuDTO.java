8e5c8fe23c1ab6021bc2f23df7378b7ab4f0100c
Federico Pantaleone <fragsman@gmail.com>
2018-08-03 10:54:51 -0300

    add Menu and MenuType DTOs

---
+++ Ratatoullie/src/main/java/components/dtos/MenuDTO.java
@@ -0,0 +1,48 @@
+package components.dtos;
+
+import java.util.List;
+
+import model.business.Dish;
+import model.business.MenuType;
+import model.business.Tag;
+
+public class MenuDTO {
+
+	private Long oid;
+	private String name;
+	private List<DishDTO> dtoDishes;
+	private List<TagDTO> dtoTags;
+	private MenuTypeDTO menuType;
+	
+	
+	public Long getOid() {
+		return oid;
+	}
+	public void setOid(Long oid) {
+		this.oid = oid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<DishDTO> getDtoDishes() {
+		return dtoDishes;
+	}
+	public void setDtoDishes(List<DishDTO> dtoDishes) {
+		this.dtoDishes = dtoDishes;
+	}
+	public List<TagDTO> getDtoTags() {
+		return dtoTags;
+	}
+	public void setDtoTags(List<TagDTO> dtoTags) {
+		this.dtoTags = dtoTags;
+	}
+	public MenuTypeDTO getMenuType() {
+		return menuType;
+	}
+	public void setMenuType(MenuTypeDTO menuType) {
+		this.menuType = menuType;
+	}
+}
