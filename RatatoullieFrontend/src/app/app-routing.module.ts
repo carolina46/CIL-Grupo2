1a4f79193d8065add2670e39cc6c80f5445dc8e3
carolina <mariacarola46@hotmail.com>
2018-08-11 23:20:03 -0300

    change for relative paths

--- RatatoullieFrontend/src/app/app-routing.module.ts
+++ RatatoullieFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ListCategoryComponent } from 'src/app/list-category/list-category.component';
-import { AppComponent } from 'src/app/app.component';
+import { ListCategoryComponent } from './list-category/list-category.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: 'listCategories', component: ListCategoryComponent },
