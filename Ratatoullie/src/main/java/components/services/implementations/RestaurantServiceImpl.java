ca108782cb231842a5403fbf58a7e6fa52b65a66
Federico Pantaleone <fragsman@gmail.com>
2018-08-27 14:07:03 -0300

    removed methods for retrieving list of filters, it is not neccessary at the moment

--- Ratatoullie/src/main/java/components/services/implementations/RestaurantServiceImpl.java
+++ Ratatoullie/src/main/java/components/services/implementations/RestaurantServiceImpl.java
@@ -1,6 +1,5 @@
 package components.services.implementations;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,17 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 import components.daos.interfaces.RestaurantDAO;
 import components.services.interfaces.RestaurantService;
 import model.business.Restaurant;
-import model.filter.ComensalCommentFilter;
-import model.filter.ComensalNotificationFilter;
-import model.filter.CommentFilter;
-import model.filter.DenyCommentFilter;
-import model.filter.DenyNotificationFilter;
-import model.filter.GourmetCommentFilter;
-import model.filter.GourmetNotificationFilter;
-import model.filter.NotificationFilter;
-import model.filter.VisitorCommentFilter;
-import model.filter.VisitorNotificationFilter;
-
 @Service
 @Transactional
 public class RestaurantServiceImpl implements RestaurantService {
@@ -63,38 +51,6 @@ public class RestaurantServiceImpl implements RestaurantService {
 	@Override
 	public List<Restaurant> getAllRestaurants() {
 		return restaurantDAO.getAll();
-	}
-
-	@Override
-	public List<CommentFilter> getCommentFilters() {
-		//I just need a list of CommentFilter classes => no need to access to DB and therefore it won't use a DAO)
-		ArrayList<CommentFilter> filters = new ArrayList<CommentFilter>();
-		DenyCommentFilter f1 = new DenyCommentFilter();
-		VisitorCommentFilter f2 = new VisitorCommentFilter();
-		ComensalCommentFilter f3 = new ComensalCommentFilter();
-		GourmetCommentFilter f4 = new GourmetCommentFilter();
-		filters.add(f1);
-		filters.add(f2);
-		filters.add(f3);
-		filters.add(f4);
-		
-		return filters;
-	}
-
-	@Override
-	public List<NotificationFilter> getNotificationFilters() {
-		//I just need a list of NotificationFilter classes => no need to access to DB and therefore it won't use a DAO)
-		ArrayList<NotificationFilter> filters = new ArrayList<NotificationFilter>();
-		DenyNotificationFilter f1 = new DenyNotificationFilter();
-		VisitorNotificationFilter f2 = new VisitorNotificationFilter();
-		ComensalNotificationFilter f3 = new ComensalNotificationFilter();
-		GourmetNotificationFilter f4 = new GourmetNotificationFilter();
-		filters.add(f1);
-		filters.add(f2);
-		filters.add(f3);
-		filters.add(f4);
-				
-		return filters;
 	}
 
 }
