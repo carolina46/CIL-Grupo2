65c14f23b57867fe1e2e23945d6aa16dd2e1ae97
Federico Pantaleone <fragsman@gmail.com>
2018-08-26 21:24:07 -0300

    modify restaurant controller for handling get comment filter

--- Ratatoullie/src/main/java/components/controllers/RestaurantController.java
+++ Ratatoullie/src/main/java/components/controllers/RestaurantController.java
@@ -1,8 +1,12 @@
 package components.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +16,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import components.JsonToDTOConverter;
+import components.dtos.business.CategoryDTO;
+import components.dtos.filter.CommentFilterDTO;
+import components.dtos.filter.NotificationFilterDTO;
 import components.services.interfaces.CategoryService;
 import components.services.interfaces.RestaurantService;
 import model.business.Category;
 import model.business.Restaurant;
+import model.filter.CommentFilter;
+import model.filter.NotificationFilter;
 
 
 
@@ -27,6 +37,8 @@ public class RestaurantController {
 	private RestaurantService restaurantService;
 	@Autowired
 	private CategoryService categoryService;
+	@Autowired
+    private ModelMapper modelMapper;
 	
 	@RequestMapping(value = "/restaurantForm", method = RequestMethod.GET)
 	public ModelAndView getRestaurantForm() {
@@ -57,6 +69,24 @@ public class RestaurantController {
 		model.setViewName("restaurantProfile");
 		model.addObject("restaurant", restaurant);
 		return model;
-	}	
+	}
+	
+	
+	@RequestMapping(value = "/getCommentFilters")
+	public ResponseEntity<String> getCommentFilters() {
+		List<CommentFilterDTO> listDTO = new ArrayList<CommentFilterDTO>();
+		List<CommentFilter> list = restaurantService.getCommentFilters();
+		
+		//Iterates the list of CommentFilter to make a list of CommentFilterDTO
+		for (CommentFilter cf : list) {
+			CommentFilterDTO commentFilterDTO = modelMapper.map(cf, CommentFilterDTO.class);
+			listDTO.add(commentFilterDTO);	
+		}
+		
+		//Converts the list of CommentFilter to JSON string
+        String jsonResult = JsonToDTOConverter.convertToJason(listDTO);
+    	
+        return new ResponseEntity<String>(jsonResult, HttpStatus.OK);
+	}
 	
 }
