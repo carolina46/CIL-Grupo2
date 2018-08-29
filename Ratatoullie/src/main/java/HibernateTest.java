1e735f14342a4ecbcaf3d5de5fed840c8bad4953
Federico Pantaleone <fragsman@gmail.com>
2018-08-29 17:06:08 -0300

    creating a admin user

--- Ratatoullie/src/main/java/HibernateTest.java
+++ Ratatoullie/src/main/java/HibernateTest.java
@@ -1,32 +1,17 @@
-import java.util.List;
-
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import components.daos.implementations.RestaurantDAOImpl;
-import model.business.Restaurant;
+import components.services.implementations.UserServiceImpl;
+import model.users.Administrator;
 
 public class HibernateTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/WEB-INF/mvc-dispatcher-servlet.xml");
-		
-		RestaurantDAOImpl resrautantDAO = context.getBean(RestaurantDAOImpl.class);
-		
-		Restaurant restaurant = new Restaurant();
-		restaurant.setName("Starbucks");
-				
-		resrautantDAO.save(restaurant);
-		
-		
-		List<Restaurant> list = resrautantDAO.getAll();
-		
-		for(Restaurant r : list){
-			System.out.println("Restaurant List::"+r);
+		Administrator a = new Administrator("Superadmin", "admin", "admin");
+		UserServiceImpl us = new UserServiceImpl();
+		if(us.getUserByUser("admin")==null) { //admin not created, proceeds to creation
+			Long oid = us.saveUser(a);
+			System.out.print("Usuario Administrador creado");
 		}
-		
-		context.close();
 	}
 
 }
