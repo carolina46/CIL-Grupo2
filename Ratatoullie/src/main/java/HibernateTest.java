5898885fa130058586f89baa8ca4633614dab376
Federico Pantaleone <fragsman@gmail.com>
2018-08-29 22:25:01 -0300

    corrected user mapping

--- Ratatoullie/src/main/java/HibernateTest.java
+++ Ratatoullie/src/main/java/HibernateTest.java
@@ -7,11 +7,14 @@ public class HibernateTest {
 		// TODO Auto-generated method stub
 		
 		Administrator a = new Administrator("Superadmin", "admin", "admin");
+		System.out.println(a);
+		
 		UserServiceImpl us = new UserServiceImpl();
-		if(us.getUserByUser("admin")==null) { //admin not created, proceeds to creation
+		System.out.println(us);
+		//if(us.getUserByUser("admin")==null) { //admin not created, proceeds to creation
 			Long oid = us.saveUser(a);
 			System.out.print("Usuario Administrador creado");
-		}
+		//}
 	}
 
 }
